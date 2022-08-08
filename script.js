952854bdcbc9a98743160cba54a19029e25d1178
NoraXu-0111 <noraxu0111@gmail.com>
2022-08-08 13:48:16 -0400

    changes

--- script.js
+++ script.js
@@ -272,7 +272,6 @@ Array.from(aboutMeTextContent).forEach((char) => {
 // Projects
 const container = document.querySelector(".container");
 const projects = document.querySelectorAll(".project");
-const projectHideBtn = document.querySelector(".project-hide-btn");
 
 projects.forEach((project, i) => {
   project.addEventListener("mouseenter", () => {
@@ -285,42 +284,8 @@ projects.forEach((project, i) => {
     project.firstElementChild.style.top = "2rem";
   });
 
-  // Big Project Image
-  project.addEventListener("click", () => {
-    const bigImgWrapper = document.createElement("div");
-    bigImgWrapper.className = "project-img-wrapper";
-    container.appendChild(bigImgWrapper);
 
-    const bigImg = document.createElement("img");
-    bigImg.className = "project-img";
-    const imgPath = project.firstElementChild.getAttribute("src").split(".")[0];
-    bigImg.setAttribute("src", `${imgPath}-big.jpg`);
-    bigImgWrapper.appendChild(bigImg);
-    document.body.style.overflowY = "hidden";
-
-    document.removeEventListener("scroll", scrollFn);
-
-    mouseCircle.style.opacity = 0;
-
-    progressBarFn(bigImgWrapper);
-
-    bigImgWrapper.onscroll = () => {
-      progressBarFn(bigImgWrapper);
-    };
-
-    projectHideBtn.classList.add("change");
-
-    projectHideBtn.onclick = () => {
-      projectHideBtn.classList.remove("change");
-      bigImgWrapper.remove();
-      document.body.style.overflowY = "scroll";
-
-      document.addEventListener("scroll", scrollFn);
-
-      progressBarFn();
-    };
-  });
-  // End of Big Project Image
+  
 
   i >= 6 && (project.style.cssText = "display: none; opacity: 0");
 });
